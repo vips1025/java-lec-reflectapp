2ea4b403c70aff96551a6938c78182a4ffc13e71
vips1025 <vips1025@gmail.com>
2024-01-23 12:04:34 +0900

    reflection ex01

---
+++ src/main/java/ex01/UserController.java
@@ -0,0 +1,16 @@
+package ex01;
+
+public class UserController {
+    // /login
+    public void login(){
+        System.out.println("login 호출");
+    }
+    // /join
+    public void join(){
+        System.out.println("join 호출");
+    }
+
+    public void userinfo(){
+        System.out.println("유저정보 보기");
+    }
+}
